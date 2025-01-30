e377ffe6294db9b61af65317ca7047b322f3a671
Roo10iuM <danil5400000@gmail.com>
2025-01-30 17:12:36 +0300

    mvp

--- app/index.tsx
+++ app/index.tsx
@@ -1,64 +1,84 @@
-import React, { useState } from 'react';
-import { Text, View, Button, TextInput, StyleSheet } from "react-native";
+import React, { useRef, useState } from 'react';
+import { Text, View, Button, TextInput, StyleSheet, Image, ImageBackground, TouchableHighlight, Dimensions, StatusBar, TouchableOpacity } from "react-native";
 
 
 
 const Player = (props: any) => {
+  const name = useRef();
+
   return (
-    <View>
+    <View style={styles.player}>
       <TextInput
         style={styles.playerName}
         placeholder={props.placeholder}
+        placeholderTextColor='white'
+        maxLength={9}
+        value={props.name}
         onChangeText={newText => props.onChangeName(newText)}
-        defaultValue={props.name}
+        onEndEditing={() => props.onChangeName(props.name.trim())}
       />
-      <Text style={{ height: 40, padding: 5 }}>
-        Health: {props.health - (parseInt(props.damage) || 0)}
+
+      <Text style={styles.health}>
+        {props.health - (parseInt(props.damage) || 0)}
       </Text>
-      <View style={{flexDirection: 'row', width: 200, height: 100}}>
-        <Button
-          onPress={() => {
-            props.onChangeHealth(props.health + 1)
-          }}
-          title="increase"
-        />
-        <Button
+
+      <View style={styles.inde}>
+        <TouchableOpacity
+          style={styles.indebutton}
           onPress={() => {
             props.onChangeHealth(props.health - 1)
           }}
-          title="decrease"
-        />
+          activeOpacity={0.7}
+        >
+          <Image style={{ width: 70, height: 70, flex: 1, resizeMode: 'contain' }} source={require('../assets/images/hpdec.png')} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.indebutton}
+          onPress={() => {
+            props.onChangeHealth(props.health + 1)
+          }}
+          activeOpacity={0.7}
+        >
+          <Image style={{ width: 100, height: 100, flex: 1, resizeMode: 'contain' }} source={require('../assets/images/hpinc.png')} />
+        </TouchableOpacity>
       </View>
+
       <TextInput
-        style={{ height: 40, padding: 5 }}
+        style={styles.damageInput}
         placeholder="Type damage here"
+        placeholderTextColor="white"
         onChangeText={newText => props.onChangeDamage(newText)}
         value={props.damage}
+        inputMode='decimal'
       />
     </View>
   )
 }
 
 const Health = (props: any) => {
-  const [playerOneName, setPlayerOneName] = useState('Player 1');
+  const [playerOneName, setPlayerOneName] = useState('Artem');
   const [playerOneHealth, setPlayerOneHealth] = useState(50);
   const [playerOneDamage, setPlayerOneDamage] = useState('');
 
-  const [playerTwoName, setPlayerTwoName] = useState('Player 2');
+  const [playerTwoName, setPlayerTwoName] = useState('Danil');
   const [playerTwoHealth, setPlayerTwoHealth] = useState(50);
   const [playerTwoDamage, setPlayerTwoDamage] = useState('');
 
   const [curTurn, setCurTurn] = useState(0);
 
   return (
-    <View style={styles.container}>
-      <Text style={{fontSize: 20, textAlign: 'center'}}>
-        Player {(curTurn + 1) % 2 ? playerOneName || 'Player 1' : playerTwoName || 'Player 2'} acts
+    <ImageBackground
+      source={require('../assets/images/space.jpg')}
+      style={styles.container}
+    >
+      <StatusBar backgroundColor='black' barStyle='dark-content'/>
+      <Text style={styles.turnBar}>
+        {((curTurn + 1) % 2 ? playerOneName || 'Player 1' : playerTwoName || 'Player 2').trim()} acts
       </Text>
 
       <View style={styles.scoreboard}>
         <Player
-          style={styles.player}
           name={playerOneName}
           onChangeName={setPlayerOneName}
           placeholder='Player 1'
@@ -69,7 +89,6 @@ const Health = (props: any) => {
         />
 
         <Player
-          style={styles.player}
           name={playerTwoName}
           onChangeName={setPlayerTwoName}
           placeholder='Player 2'
@@ -79,8 +98,9 @@ const Health = (props: any) => {
           onChangeDamage={setPlayerTwoDamage}
         />
       </View>
-
-      <Button
+      <View style={{ flexGrow: 1 }}></View>
+      <TouchableOpacity
+        style={styles.turnButton}
         onPress={() => {
           setCurTurn(curTurn + 1)
 
@@ -90,33 +110,93 @@ const Health = (props: any) => {
           setPlayerTwoHealth(playerTwoHealth - (parseInt(playerTwoDamage) || 0))
           setPlayerTwoDamage('')
         }}
-        title='next turn'
-      />
-    </View>
+      >
+        <Text style={styles.textTurnButton}>NEXT TURN</Text>
+      </TouchableOpacity>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'space-between',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    //flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
     padding: 10,
+  },
+  turnBar: {
+    fontSize: 40, 
+    textAlign: 'center', 
+    color: 'white', 
+    borderBottomWidth: 1, 
+    borderBottomColor: 'white', 
+    padding: 5 
   },
   scoreboard: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    justifyContent: 'center',
+    marginTop: 50,
   },
   player: {
     flexGrow: 1,
-    margin: 10,
+    margin: 5,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: 'green'
   },
   playerName: {
     fontSize: 40,
+    color: 'white',
     textAlign: 'center',
   },
-  HD: {
-    width: 100, 
+  health: {
+    color: 'white',
+    fontSize: 80,
+    textAlign: 'center',
+  },
+  inde: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    width: 200,
     height: 100,
+  },
+  indebutton: {
+    flex: 1,
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: 'red',
+  },
+  damageInput: {
+    color: 'white',
+    backgroundColor: 'rgba(0, 0, 0, 0.67)',
+    width: 200,
+    //minHeight: 40,
+    fontSize: 18,
+    marginTop: 10,
+    padding: 5,
+    textAlign: 'center',
+  },
+  turnButton: {
+    backgroundColor: 'rgba(0, 0, 0, 0.67)', 
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 3,
+    borderColor: 'white',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  textTurnButton: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
 
